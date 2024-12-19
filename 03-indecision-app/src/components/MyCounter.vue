adefc216293cbaf2eb0d531a99898c0da81a76d1
Jhon <jhon.contreras13@gmail.com>
2024-12-18 22:42:07 -0300

    Primer componente

---
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <section>
+        <h3>Counter:{{ counter }}</h3>
+        <h3>Square: {{ squareCounter }}</h3>
+
+        <div>
+            <button @click="incrementOne">+1</button>
+            <button @click="decrementOne">-1</button>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+    import { ref, computed } from 'vue';
+    const counter = ref(2);
+    
+    const incrementOne = () => {
+        counter.value = counter.value + 1;
+    }
+    const decrementOne = () => {
+        counter.value = counter.value - 1;
+    }
+
+
+    const squareCounter = computed( ()=> counter.value * counter.value)
+
+</script>
+
+
+
+<style scoped>
+
+</style>
